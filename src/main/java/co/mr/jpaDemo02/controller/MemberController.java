a1a58f2a81df71ef1496c7de1d05cd091197a434
mrofftin <mrofftin@gmail.com>
2022-02-01 12:04:20 +0900

    회원가입 기능 구현완료

---
+++ src/main/java/co/mr/jpaDemo02/controller/MemberController.java
@@ -0,0 +1,52 @@
+package co.mr.jpaDemo02.controller;
+
+import co.mr.jpaDemo02.dto.MemberFormDto;
+import co.mr.jpaDemo02.entity.Member;
+import co.mr.jpaDemo02.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+    @PostMapping("/new")
+    // 검증 대상 객체에 @Valid를 붙여주고, 검사결과를 담아놓을 BindingResult 객체를 파라미터로 추가
+    public String memberForm(@Valid MemberFormDto memberFormDto,
+                             BindingResult bindingResult, Model model) {
+        // bindingResult.hasErrors()호출하여 에러가 있으면 가입페이지로 이동
+        System.out.println("binddingResult 결과 : " +bindingResult.hasErrors());
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            // 에러 메시지를 뷰로 전달
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        // 가입 성공하면 home으로
+        return "redirect:/";
+    }
+}
--- src/main/java/co/mr/jpaDemo02/dto/MemberFormDto.java
+++ src/main/java/co/mr/jpaDemo02/dto/MemberFormDto.java
@@ -2,16 +2,27 @@ package co.mr.jpaDemo02.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 // 회원 가입화면으로 부터 넘어오는 가입정보를 담을 dto를 생성
 @Getter @Setter
 public class MemberFormDto {
+    @NotBlank(message = "* 필수 입력값") // null 체크, 문자열의 경우 길이 0 및 문자열이 " " 검사
     private String name;
 
+    @NotEmpty(message = "* 필수 입력값")
+    @Email(message = "이메일 형식에 맞게 입력하세요.")
     private String email;
 
+    @NotEmpty(message = "* 필수 입력값")
+    @Length(min=8, max=20, message = "8자이상, 20자 이하로 입력하세요.")
     private String password;
 
+    @NotEmpty(message = "* 필수 입력값")
     private String address;
 }
 
